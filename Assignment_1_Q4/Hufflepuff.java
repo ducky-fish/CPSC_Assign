ee01a53727c4b644b43e9aa2b5aad1bd16736b2e
ducky-fish <35616959+ducky-fish@users.noreply.github.com>
2018-10-04 17:14:52 -0600

    Update Hufflepuff.java

--- Assignment_1_Q4/Hufflepuff.java
+++ Assignment_1_Q4/Hufflepuff.java
@@ -1,39 +1,61 @@
 package cpsc331.A1;
 public class Hufflepuff{
 	public static void main(String args[]) {
-		SHufflepuff user = new SHufflepuff();
 		String user_input = args[0];			    	
-    	boolean valid_input = SHufflepuff.isInt(user_input);
+    		boolean valid_input = Hufflepuff.isInt(user_input);
     if(valid_input == true && args.length == 1){							//Scans input to see if argument is int           		
        		int user_int = Integer.parseInt(user_input);
        		if(user_int >= 0) {
        			int result = eval(user_int);
        			System.out.print(result + "\n");
        		}
-       		
+	    
        		else {
        			System.out.println("Silly Muggle! The input integer cannot be negative.");	//error message for if the input is negative
        		}
-  		}
+  	}
        	
-  	else {
-  		System.out.println("Silly Muggle! One integer input is required.");					//error message for if the input is not an integer
+  else {
+  	System.out.println("Silly Muggle! One integer input is required.");					//error message for if the input is not an integer
   	}																						//or if the input is more or less than 1
 
  }
 	
 	public static int eval(int n) {
-		int h = 0;
-		while(h <= 3) {
-			if(n==h) {
+		int c = 0;
+		while(c <= 3) {
+			if(n==c) {
 				int final_reply = 10-n;
 				return final_reply;
 			}
-			h++;
+			c++;
 		  }
+		int i = 3;
+		int h = 10;
+		int p = 9;
+		int a = 8;
+		int k = 7;
 		
+		while(i<n){
+			int result = 4 * k - 6 * a + 4 * p - h;
+			h =p;
+			p=a;
+			a=k;
+			k=result;
+			i = i+1;
+		}
+		return k;
 	}
-	
+
+	private static boolean isInt(String s){
+		try{
+			Integer.parseInt(s);
+			return true;
+		}
+		catch(NumberFormatException e){
+			return false;
+		}
+	}
 }
 	
 	
